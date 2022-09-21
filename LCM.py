64cd0f5591c1b6964339b482697bda93e1bf3d6f
rajeshmaradani <96291809+rajeshmaradani@users.noreply.github.com>
2022-09-21 21:03:16 +0530

    def lcm(x,y):     if x>y:         greater=x     else:         greater=y     while(True):         if ((greater%x==0) and (greater%y==0)):             lcm=greater             break         greater+=1
        return lcm
    
    n,m=map(int,input().split())
    print(lcm(n,m))

---
+++ LCM.py
@@ -0,0 +1,15 @@
+def lcm(x,y):
+    if x>y:
+        greater=x
+    else:
+        greater=y
+    while(True):
+        if ((greater%x==0) and (greater%y==0)):
+            lcm=greater
+            break
+        greater+=1
+   
+    return lcm
+
+n,m=map(int,input().split())
+print(lcm(n,m))
