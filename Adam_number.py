b03ac5735fcbd345640d1d80971ce49a1b133181
rajeshmaradani <96291809+rajeshmaradani@users.noreply.github.com>
2022-11-02 21:05:38 +0530

    def rev(n):     t=n     rev=0     while n:         r=n%10         rev=rev*10+r         n=n//10     return rev
    n=int(input())
    m=n*n
    o=rev(n)
    q=o*o
    p=rev(q)
    if p==m:
        print("True")
    else:
        print("False")

--- Adam_number.py
+++ Adam_number.py
@@ -1,16 +1,20 @@
 def rev(n):
-    rem=0
+    t=n
+    rev=0
     while n:
         r=n%10
-        rem=rem*10+r
+        rev=rev*10+r
         n=n//10
-    return rem
+    return rev    
+
+
 n=int(input())
-m=n**2
-r1=rev(n)
-r2=r1**2
-r3=rev(r2)
-if m==r3:
+m=n*n
+o=rev(n)
+q=o*o
+p=rev(q)
+if p==m:
     print("True")
 else:
     print("False")
+
