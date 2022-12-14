cd7748d519fec74d98d7169de4a230e2e5557769
rajeshmaradani <96291809+rajeshmaradani@users.noreply.github.com>
2022-12-14 22:00:04 +0530

    def count(n):     c=0     while n:         r=n%10         c+=1         n//=10     return c n=int(input()) r=[] c=0 x=list(map(int,input().split())) for i in x:     r.append(count(i)) p=min(r) for i in r:     if i==p:         c+=1 print(c)

---
+++ Minimum_digit_elements.py
@@ -0,0 +1,18 @@
+def count(n):
+    c=0
+    while n:
+        r=n%10
+        c+=1
+        n//=10
+    return c
+n=int(input())
+r=[]
+c=0
+x=list(map(int,input().split()))
+for i in x:
+    r.append(count(i))
+p=min(r)
+for i in r:
+    if i==p:
+        c+=1
+print(c)
