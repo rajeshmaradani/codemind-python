e92462720aa2ef32af03bea8893a72a0fbe428a0
rajeshmaradani <96291809+rajeshmaradani@users.noreply.github.com>
2022-12-14 21:19:51 +0530

    def rowsum(d,r,c):     res=[0 for i in range(r)]     for i in range(r):         for j in range(c):             res[i]+=d[i][j]     return res
    r,c=map(int,input().split())
    row=[0 for i in range(c)]
    d=[row.copy() for i in range(r)]
    for i in range(r):
        vals=list(map(int,input().split()))
        for j in range(c):
            d[i][j]=vals[j]
    
    res=rowsum(d,r,c)
    print(max(res))

---
+++ Maximum_sum_of_row_wise_sum_of_a_matrix.py
@@ -0,0 +1,17 @@
+def rowsum(d,r,c):
+    res=[0 for i in range(r)]
+    for i in range(r):
+        for j in range(c):
+            res[i]+=d[i][j]
+    return res        
+
+r,c=map(int,input().split())
+row=[0 for i in range(c)]
+d=[row.copy() for i in range(r)]
+for i in range(r):
+    vals=list(map(int,input().split()))
+    for j in range(c):
+        d[i][j]=vals[j]
+
+res=rowsum(d,r,c)
+print(max(res))
