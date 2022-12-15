f563e203e4e44b8024faf58e466f0cdb61f69783
rajeshmaradani <96291809+rajeshmaradani@users.noreply.github.com>
2022-12-15 23:25:16 +0530

    def count(s):     c=0     for i in s:         if i in "aeiouAEIOU":             c+=1     return c s=input() s1=s.split() r=[] c=0 for i in s1:     r.append(count(i)) r1=max(r) for i in r:     if i==r1:         c+=1 print(c)

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,17 @@
+def count(s):
+    c=0
+    for i in s:
+        if i in "aeiouAEIOU":
+            c+=1
+    return c
+s=input()
+s1=s.split()
+r=[]
+c=0
+for i in s1:
+    r.append(count(i))
+r1=max(r)
+for i in r:
+    if i==r1:
+        c+=1
+print(c)
