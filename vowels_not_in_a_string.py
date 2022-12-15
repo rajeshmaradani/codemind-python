e98bbf5513ebd5155a62f948861184b41bf9aaad
rajeshmaradani <96291809+rajeshmaradani@users.noreply.github.com>
2022-12-15 22:19:54 +0530

    s=input() r=[] c=0 for j in s:     if j=="a" or j=="e" or j=="i" or j=="o"or j=="u":         r.append(j) r1=set(r) m="aeiou" for i in m:     if i not in r1:         c+=1         print(i,end=" ") if c==0:     print(0)

---
+++ vowels_not_in_a_string.py
@@ -0,0 +1,14 @@
+s=input()
+r=[]
+c=0
+for j in s:
+    if j=="a" or j=="e" or j=="i" or j=="o"or j=="u":
+        r.append(j)
+r1=set(r)
+m="aeiou"
+for i in m:
+    if i not in r1:
+        c+=1
+        print(i,end=" ")
+if c==0:
+    print(0)
